372380e8368bea463ebbb81d81627ffee871228e
YLM <670444337@qq.com>
2015-04-24 11:34:20 +0800

    add xutils

---
+++ app/src/main/java/com/mythos/demo/ui/XUtilsActivity.java
@@ -0,0 +1,18 @@
+package com.mythos.demo.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mythos.demo.R;
+
+public class XUtilsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xutils);
+    }
+
+}
